685b66dafc3ce96b7880b3249da7c1d2198fd872
Arthur Jen <arthurljen@gmail.com>
2018-08-29 18:27:57 -0700

    fixed pathing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -29,7 +29,7 @@ class App extends Component {
           <main>
             <Switch>
               <Route exact path="/" component={Home}/>
-              <Route exact path="games/:gameKey" component={Game}/>
+              <Route exact path="/games/:gameKey" component={Game}/>
               <Redirect to="/"/>
             </Switch>
           </main>
